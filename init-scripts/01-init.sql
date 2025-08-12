9fcb9d686fd3efb8e619563b1ce361c1d27d28ac
Guilherme Mello <guilherme.araujo@cilia.com.br>
2025-08-12 18:26:25 -0300

    initial

---
+++ init-scripts/01-init.sql
@@ -0,0 +1,8 @@
+-- Script de inicialização do banco de dados
+-- Este arquivo será executado automaticamente quando o container subir pela primeira vez
+
+-- Criar extensões se necessário
+-- CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+-- Aqui você pode adicionar tabelas iniciais, dados de seed, etc.
+-- Por enquanto, deixamos vazio pois o Hibernate criará as tabelas automaticamente
